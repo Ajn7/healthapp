1ee1ecc5fad54b405b7a38e06497c96ff6907417
Ajn7 <arjuntrcr7@gmail.com>
2023-02-15 10:19:05 +0530

    first commit

---
+++ lib/constants/headline.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Widget headline(){
+  return Align(
+   alignment: Alignment.topCenter,
+   child:Padding(
+    padding: EdgeInsets.all(76.0),
+    child:Text('HealthConnect',style: TextStyle(color:Color(0xFFB80075),fontSize: 35,fontWeight: FontWeight.bold)),)
+);
+
+}
+
+
+

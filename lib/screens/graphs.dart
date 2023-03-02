2387d982b7914e5af44e973d60db63a7469069ce
Ajn7 <arjuntrcr7@gmail.com>
2023-03-02 11:09:15 +0530

    added dynamic grph

--- lib/screens/graphs.dart
+++ lib/screens/graphs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../constants/headline.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 class Graphscreen extends StatefulWidget {
    const Graphscreen({super.key});
@@ -11,10 +10,7 @@ class _GraphscreenState extends State<Graphscreen> {
   
  //Map<int,int> Spo={1:2,6:5,3:5};
  
-//List<FlSpot> dummyData1 =Spo.values.toList();
-//List<int> value=[1,2,3,4];
-// double a=2;
-// double b=5;
+
   
   late TooltipBehavior _tooltipBehavior;
 
@@ -26,7 +22,7 @@ class _GraphscreenState extends State<Graphscreen> {
 
   @override
   Widget build(BuildContext context) {
-    
+
     return Scaffold(
           backgroundColor: Colors.white,
           appBar: AppBar(title: const Text('HealthConnect',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold))),
@@ -34,55 +30,40 @@ class _GraphscreenState extends State<Graphscreen> {
             child:Column(
             children: [
               //headline(),
-            Text('Previo Reading :'),
+            const Text('Previo Reading :'),
              SizedBox(
               width: 300,
               height: 300,
-               child: Expanded(
-                 child: ListView(
-                   physics: BouncingScrollPhysics(),
-                   scrollDirection: Axis.horizontal,
-                   children:[ 
-                    Container(
+               child: ListView(
+                 physics:const BouncingScrollPhysics(),
+                 scrollDirection: Axis.horizontal,
+                 children:[ 
+                  SizedBox(
               
-                          child: SfCartesianChart(
-                              // Initialize category axis
-                              title:  ChartTitle(
-                              text: 'SPO2',
-                              //backgroundColor: Colors.lightGreen,
-                              //borderColor: Colors.blue,
-                              //borderWidth: 2,
-                              // Aligns the chart title to left
-                              alignment: ChartAlignment.center,
-                              textStyle:const TextStyle(
-                              color: Colors.red,
-                              fontFamily: 'Roboto',
-                              fontStyle: FontStyle.italic,
-                              fontSize: 20,)
-                               ),
-                               // Enables the tooltip for all the series in chart
-                              tooltipBehavior: _tooltipBehavior,
-                              primaryXAxis: CategoryAxis(),
-                              series: <ChartSeries>[
-                                  // Initialize line series
-                                  LineSeries<ChartData, String>(
-                                      dataSource: [
-                                          // Bind data source
-                                          ChartData('Jan', 100),
-                                          ChartData('Feb', 99),
-                                          ChartData('Mar', 80),
-                                          ChartData('Apr', 72),
-                                          ChartData('May', 60)
-                                      ],
-                                      xValueMapper: (ChartData data, _) => data.x,
-                                      yValueMapper: (ChartData data, _) => data.y,
-                                      dataLabelSettings:const DataLabelSettings(isVisible : true)
-                                  )
-                              ]
-                          )
-                      ),
-                   ],
-                 ),
+                        child: SfCartesianChart(
+                            // Initialize category axis
+                            title:  ChartTitle(
+                            text: 'SPO2',
+                            //backgroundColor: Colors.lightGreen,
+                            //borderColor: Colors.blue,
+                            //borderWidth: 2,
+                            // Aligns the chart title to left
+                            alignment: ChartAlignment.center,
+                            textStyle:const TextStyle(
+                            color: Colors.red,
+                            fontFamily: 'Roboto',
+                            fontStyle: FontStyle.italic,
+                            fontSize: 20,)
+                             ),
+                             // Enables the tooltip for all the series in chart
+                            tooltipBehavior: _tooltipBehavior,
+                            primaryXAxis: CategoryAxis(),
+                            series: <ChartSeries>[
+                                getData()
+                            ]
+                        )
+                    ),
+                 ],
                ),
              )
             ],
@@ -91,48 +72,33 @@ class _GraphscreenState extends State<Graphscreen> {
   );
  }
 }
- class ChartData {
+ 
+
+LineSeries<ChartData, String> getData() {
+
+  List<ChartData> spData=[];
+
+    int i;
+    List<String>mnth=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
+    List<double>dt=[10,20,30,40,50,60,70,80,90,100,110,120];
+    for(i=0;i<dt.length;i++){
+      spData.add(ChartData(mnth[i],dt[i]));
+    }
+
+  return LineSeries<ChartData, String>(
+
+    // Create a new LineSeries object
+    dataSource:spData,
+    xValueMapper: (ChartData data, _) => data.x, // Map x values to ChartData.x
+    yValueMapper: (ChartData data, _) => data.y, // Map y values to ChartData.y
+    dataLabelSettings:const DataLabelSettings(isVisible : true)
+
+  );
+
+}
+
+class ChartData {
         ChartData(this.x, this.y);
         final String x;
         final double? y;
     }
-
-
-
-//import 'package:fl_chart/fl_chart.dart';
-//import 'dart:math';
-
-// final List<FlSpot> dummyData1 = List.generate(8, (index) {
-//     return FlSpot(index.toDouble(), index * Random().nextDouble());
-
-//   });
-// Container(
-//         padding: const EdgeInsets.all(10),
-//         width: double.infinity,
-//         height: 300,
-//         child: LineChart(
-//           LineChartData(
-//             borderData: FlBorderData(show: false),
-//            lineBarsData: [
-//             LineChartBarData(
-//               spots:dummyData1,
-//                //[ 
-//               // const FlSpot(0, 1),
-//               // const FlSpot(1, 3),
-//               // const FlSpot(2, 10),
-//               // const FlSpot(3, 7),
-//               // const FlSpot(4, 12),
-//               // const FlSpot(5, 13),
-//               // const FlSpot(6, 17),
-//               // const FlSpot(7, 15),
-//               // const FlSpot(8, 20)
-//             //],
-//             isCurved: true,
-//             barWidth: 3,
-//             color: Colors.green,
-//             )
-//           ]
-//           ),
-//           swapAnimationDuration: Duration(milliseconds: 150),
-//         ),
-// ),
